618943af62e79c535a35ec451317ff7c1cff7259
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2023-05-26 20:50:24 +0900

    23.05.26 LV.2

---
+++ 월간 코드 챌린지 시즌1/이진 변환 반복하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = ""
+    cnt = 0
+    zero = 0
+    while True:
+        if s== "1":
+            break
+        zero += s.count('0')
+        s = s.replace('0', "")
+        s = bin(len(s))
+        s = s[2: ]
+        cnt+=1
+    answer = [cnt, zero]
+    return answer
