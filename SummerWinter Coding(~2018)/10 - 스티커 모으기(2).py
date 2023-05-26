29ad9ee165b7b4bf28c9695fc501cd68a539dc3d
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2023-05-26 16:51:56 +0900

    23.05.26 LV.3

--- SummerWinter Coding(~2018)/10 - 스티커 모으기(2).py
+++ SummerWinter Coding(~2018)/10 - 스티커 모으기(2).py
@@ -3,11 +3,13 @@ def solution(sticker):
     if len(sticker) == 1:
         return sticker[0]
     
-    dp1 = [0]*len(sticker)
-    dp2 = [0]*len(sticker)
+    dp1 = [0]*len(sticker) # 첫 번째 스티커를 뜯었을 경우
+    dp2 = [0]*len(sticker) # 첫 번째 스티커를 뜯지 않았을 경우
     
     dp1[0], dp1[1] = sticker[0], sticker[0]
     for i in range(2, len(sticker)-1):
+        # 1. i-1번 스티커를 뜯을 경우 -> i번째 스티커는 뜯을 수 없음
+        # 2. i-1번 스티커를 뜯지 않을 경우 -> i-2번까지 최대값 + i번째 스티커
         dp1[i] = max(dp1[i-2]+sticker[i], dp1[i-1])
         
     dp2[1] = sticker[1]
